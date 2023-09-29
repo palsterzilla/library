e53196415c7e09b3fb9a8c1437f41d91bfbe8110
Firyal Ghassari <naufal.firyal@gmail.com>
2023-09-30 00:49:48 +0700

    Implement addBookToLibrary function

--- app.js
+++ app.js
@@ -3,16 +3,57 @@ const preventClose = document.getElementById("preventClose");
 const bookDialog = document.getElementById("addDialog");
 const submitBtn = bookDialog.querySelector("#submitBtn");
 
+const cardWrapper = document.querySelector(".wrapper");
+const inputTitle = document.getElementById("title");
+const inputAuthor = document.getElementById("author");
+const inputPages = document.getElementById("pages");
+const readCheck = document.getElementById("readCheck");
+
 const myLibrary = [];
 
-function Book(title, author, pages) {
-  this.title = title
-  this.author = author
-  this.pages = pages
+function Book(title, author, pages, readCheck) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.readCheck = readCheck;
 }
 
 function addBookToLibrary() {
-  // do stuff here
+  const div = document.createElement("div");
+  const paraTitle = document.createElement("p");
+  const paraAuthor = document.createElement("p");
+  const paraPages = document.createElement("p");
+  const btnRead = document.createElement("BUTTON");
+  const btnRemove = document.createElement("BUTTON");
+
+  myLibrary.push(
+    new Book(
+      inputTitle.value,
+      inputAuthor.value,
+      inputPages.value,
+      readCheck.checked,
+    )
+  );
+
+  for (let i = 0; i < myLibrary.length; i++) {
+    div.classList.add("card");
+    paraTitle.textContent = `"${myLibrary[i].title}"`;
+    paraAuthor.textContent = myLibrary[i].author;
+    paraPages.textContent = `${myLibrary[i].pages} pages`;
+
+    if (readCheck.checked) {
+      btnRead.textContent = "Read";
+    } else {
+      btnRead.textContent = "Not read";
+    }
+    btnRead.setAttribute("type", "button");
+
+    btnRemove.textContent = "Remove";
+    btnRemove.setAttribute("type", "button");
+    
+    cardWrapper.append(div);
+    div.append(paraTitle, paraAuthor, paraPages, btnRead, btnRemove);
+  }
 }
 
 addBookBtn.addEventListener("click", () => {
@@ -27,13 +68,15 @@ preventClose.addEventListener('click', (event) => {
   event.stopPropagation();
 });
 
-bookDialog.addEventListener("close", (e) => {
-  document.querySelectorAll("input")[0].value = "";
-  document.querySelectorAll("input")[1].value = "";
-  document.querySelectorAll("input")[2].value = "";
-  document.querySelectorAll("input")[3].checked = false;
+bookDialog.addEventListener("close", () => {
+  inputTitle.value = "";
+  inputAuthor.value = "";
+  inputPages.value = "";
+  readCheck.checked = false;
 });
 
 submitBtn.addEventListener("click", (event) => {
-  event.preventDefault();
+  event.preventDefault();  
+  addBookToLibrary();
+  bookDialog.close();
 });
